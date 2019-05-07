5759f7611fc977318c30822936087541d3ff8eae
Baochuan Lu <blu@sbuniv.edu>
2019-05-07 15:26:33 +0000

    initial checkin

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+/*
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
+*/
+
+var port = process.env.PORT;
+var IP = process.env.IP;
+app.listen(port, IP, () => {
+  console.log('Listening on port '+process.env.PORT);
+});
